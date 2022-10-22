0fe92a20ccff490d70fe732b0e1d8a90122b408b
Ibrahim Sedhom <ibrahimsedhom@gmail.com>
2022-10-22 21:08:15 +0200

    Create login

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -14,16 +14,44 @@ class UserController {
         return res.status(400).json("All input is required");
       }
 
-      //Encrypt user password
-      //const encryptedPassword = await bcrypt.hash(password, 10);
-
       // Create token
-      const access_token = jwt.sign({ username }, config.TOKEN_KEY, {
+      const token = jwt.sign({ user_id: username }, config.TOKEN_KEY, {
         expiresIn: "1h",
       });
 
+      //Here implement Database
+      // 1- find the user in DB
+      // 2- check that user doesnt exists
+      // 3- Encrypt user password
+      // const encryptedPassword = await bcrypt.hash(password, 10);
+      // 4- save in db
+
       // return new user
-      return res.status(200).json({ access_token: access_token });
+      return res.status(200).json({ token: token });
+    } catch (error) {
+      return res.status(500);
+    }
+  };
+
+  // Login
+  static login = async (req: Request, res: Response) => {
+    try {
+      //Get parameters from the body
+      let { username, password } = req.body;
+
+      //Validade if the parameters are ok
+      if (!(username && password)) {
+        return res.status(400).json("All input is required");
+      }
+
+      //Here implement cycle login
+      // check that user exists in DB
+      // check that the password matches
+      // bcrypt.compare(password, db.password)
+
+      // Create token
+      const token = jwt.sign({ user_id: username }, config.TOKEN_KEY);
+      return res.status(200).json({ token: token });
     } catch (error) {
       return res.status(500);
     }
--- src/routes.ts
+++ src/routes.ts
@@ -4,4 +4,6 @@ const router = Router();
 
 router.post("/user", UserController.newUser);
 
+router.post("/login", UserController.login);
+
 export default router;
