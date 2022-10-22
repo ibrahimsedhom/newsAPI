6253706a443feb0c228c97ca870783e212f87bb7
Ibrahim Sedhom <ibrahimsedhom@gmail.com>
2022-10-23 00:44:28 +0200

    Create Fun Search In News

--- src/controllers/newsController.ts
+++ src/controllers/newsController.ts
@@ -3,10 +3,13 @@ import axios from "axios";
 
 import config from "../config/config";
 class UserController {
-  // Create New User
+  // Create Fetch All News
   static fetchAllNews = async (req: Request, res: Response) => {
     try {
+      // URl third party
       let URL = `https://newsapi.org/v2/top-headlines?country=eg&sortBy=publishedAt&apiKey=${config.apiKey}`;
+
+      // response third party
       const resNews: object = await callAxios(URL);
 
       return res.status(200).json(resNews);
@@ -15,15 +18,27 @@ class UserController {
     }
   };
 
-  // Login
+  // Search by in news
   static searchInNews = async (req: Request, res: Response) => {
     try {
+      const { search } = req.body;
+      if (!search) {
+        return res.status(400).json("The Search input is required");
+      }
+
+      // URl third party
+      let URL = `https://newsapi.org/v2/everything?q=${search}&sortBy=publishedAt&apiKey=${config.apiKey}`;
+
+      // response third party
+      const resNews: object = await callAxios(URL);
+
+      return res.status(200).json(resNews);
     } catch (error) {
-      return res.status(500);
+      return res.status(500).send(error);
     }
   };
 }
-
+// Call Axios
 const callAxios = async (url: string): Promise<object> => {
   try {
     const response = await axios.get(url);
